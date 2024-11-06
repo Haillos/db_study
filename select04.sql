7412c407be4a846e29c9630772d1609f68c698ec
Han-kyu <kwakbab9@gmail.com>
2024-11-06 16:57:19 +0900

    복수행함수 2

--- select04.sql
+++ select04.sql
@@ -61,6 +61,89 @@ from student
 group by grade
 order by height;
 
+--HAVING
+
+SELECT DEPTNO, AVG(sal)
+FROM emp
+group by deptno
+--where AVG(sal >= 2000)
+group by deptno
+having AVG(sal) >= 2000
+;
+1) 급여가 1500 이상인 직원들만 대상으로, 부서별 평균 급여
+select deptno, AVG(sal)
+from emp
+where sal >= 1500
+group by deptno
+;
+2) 부서별 평균 급여가 1500 이상인 정보만 보여주기
+select deptno, AVG(sal)
+from emp
+group by deptno
+having AVG(sal) >= 1500
+;
+
+SELECT
+grade,
+AVG(weight)
+FROM student
+where grade <> 4
+group by grade
+having avg(weight) >= 65
+;
+select deptno, job, round(avg(sal), 1)
+from emp
+group by deptno, job;
+
+select deptno, job, round(avg(sal), 1)
+from emp
+group by ROLLUP(deptno, job);
+
+SELECT * 
+FROM emp
+where job = 'SALESMAN' and deptno = 30;
+
+group by ROLLUP(deptno, job);
+1) deptno, job 그룹화
+2) deptno 그룹화 계
+3) () 그룹화 계
+4  ()계
+
+select deptno, job, mgr, ROUND(AVG(sal), 1)
+from emp
+group by ROLLUP(deptno, job, mgr);
+
+select deptno, job, round(avg(sal), 1)
+from emp
+group by ROLLUP((deptno, job)); 
+-- 괄호로 묶으면
+1) deptno, job 그룹화
+2) () 그룹화 계
+
+--RANK DENSE RANK
+SELECT 
+ename, 
+sal, 
+RANK() OVER (order by sal desc),
+DENSE_RANK() OVER (ORDER BY sal desc)
+FROM emp;
+--order by sal;
+
+SELECT 
+    name, 
+    height, 
+    RANK() OVER (order by height desc) 순위 1,
+    DENSE_RANK() OVER (ORDER BY height desc) 순위 2,
+    grade,
+    RANK() OVER (partition by grade order by height desc) 순위 3,
+    DENSE_RANK() OVER (partition by grade order by height desc) 순위 4
+FROM student
+order by grade, height desc;
+
+
+
+
+
 
 
 
